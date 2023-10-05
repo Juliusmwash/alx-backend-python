2b61a7109170cbe73a88be3cc7c6d3962e5559a3
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-05 22:52:40 +0300

    Commit files for the fourth task

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module defines the type-annotated function 'to_str'.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    A type-annotated function to_str that takes a float as an argument and
+    returns the string representation of the float.
+    Args:
+        n(float) - type-annotated argument.
+    """
+    return str(n)
