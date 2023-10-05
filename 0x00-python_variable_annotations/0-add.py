e56e560c2f57ff79a3366d423c74effc81ba2bb9
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-05 22:25:21 +0300

    Initialize the type annotation project

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module defines type-annotated function 'add'.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    A type-annotated function that return sum as float type
+    Args:
+        a - First type-annotated argument
+        b - second type-annotated argument
+    """
+    return a + b
