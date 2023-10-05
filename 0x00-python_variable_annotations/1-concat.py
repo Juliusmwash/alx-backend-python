1f262cf979ecf42781b912fb02eda88478bc5bc7
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-05 22:32:11 +0300

    Commit files for the second task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module defines a type-annotated function 'concat'
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type-annotated function that returns a concatenated string
+    Args:
+        str1(string) - First typw-annotated argument
+        str2(string) - Second type-annotated argument
+    """
+    return str1 + str2
