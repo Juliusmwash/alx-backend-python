f67afbd559f2707d32a7fdb338a409636c1a58fb
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-10 04:10:51 +0300

    Fix a minor type annotation error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     Asynchronous coroutine that yields random numbers between 0
     and 10 after waiting 1 second asynchronously.
